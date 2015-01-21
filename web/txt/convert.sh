853287e0624d3282b7c0255f7f3a35a68761e244
Juzef <juzefwt@gmail.com>
2015-01-21 18:59:26 +0100

    Porządek w tekstach

--- web/txt/convert.sh
+++ web/txt/convert.sh
@@ -2,7 +2,7 @@
 nb = 1
 for f in *.txt
 do
-  echo "CCL-ing $f file $nb"
-  wcrft-app nkjp.ini -i text -o ccl "$f" -O "../ccl/text$nb.xml"
+  echo "CCL-ing $f file into $f.xml"
+  wcrft-app nkjp.ini -i text -o ccl "$f" -O "../ccl/$f.xml"
   nb=$(($nb + 1))
 done 
