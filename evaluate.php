801bd12055e594f1f28b0fa266f49d8651794308
Juzef <juzefwt@gmail.com>
2015-02-09 00:29:56 +0100

    poprawki w skrypcie oceniającym

--- evaluate.php
+++ evaluate.php
@@ -1,7 +1,42 @@
 <?php
 
+if ($handle = opendir(__DIR__.'/web/ccl/')) {
+    while (false !== ($fileName = readdir($handle))) {
+        if (!in_array($fileName, array(".", "..", "extracts.xml", "extracts.xml.clean", "prepare.php"))) {
+
+            $chunkList = new SimpleXMLElement(file_get_contents(__DIR__.'/web/ccl/'.$fileName));
+            $numberWords = 0;
+            $numberSentences = 0;
+
+            foreach ($chunkList as $paragraph) {
+                foreach ($paragraph as $sentence) {
+                    $numberSentences++;
+
+                    foreach ($sentence as $tok) {
+                        $orth = (string)$tok->orth;
+                        $base = (string)$tok->lex->base;
+
+                        if ((string)$tok->lex->ctag == 'interp') {
+                            continue;
+                        }
+
+                        if (trim($base) == '' || strlen(trim($base)) < 2) {
+                            continue;
+                        }
+
+                        $numberWords++;
+                    }
+                }
+            }
+
+            echo $fileName.' - words: '.$numberWords.', sentences: '.$numberSentences.', avg: '.($numberWords/$numberSentences).PHP_EOL;
+        }
+    }
+}
+
 $extractList = new SimpleXMLElement(file_get_contents(__DIR__.'/web/ccl/extracts.xml'));
 $modelExtracts = array();
+$scores = array();
 
 foreach ($extractList as $extract) {
     $docName = (string)$extract['doc'];
@@ -9,7 +44,7 @@ foreach ($extractList as $extract) {
 
     $processedFile = __DIR__.'/web/txt/'.$docName;
     
-    if (empty($modelSentences) || !file_exists($processedFile)) {
+    if (!isset($modelSentences[1]) || !file_exists($processedFile)) {
         continue;
     }
 
@@ -40,7 +75,8 @@ foreach ($modelExtracts as $docName => $models) {
 
     file_put_contents($clusterDirPath.'/TEXT1.txt', file_get_contents($processedFile));
 
-    $cmd = sprintf('php %s %s', $scriptPath, $clusterDirPath);
+    $mode = 'default';
+    $cmd = sprintf('php %s %s %s', $scriptPath, $clusterDirPath, $mode);
 
     echo shell_exec($cmd);
 
@@ -86,8 +122,23 @@ foreach ($modelExtracts as $docName => $models) {
     $evalResultPath = __DIR__.'/web/eval/'.$docName;
 
     echo shell_exec('perl /usr/local/share/mead/bin/evaluation/rouge/rouge.pl '.$evalPath.'/system.txt '.implode(' ', $modelPaths));
-    echo shell_exec('cd /usr/local/share/mead/rouge/kraken && perl /usr/local/share/mead/rouge/ROUGE-1.5.5.pl -e "/usr/local/share/mead/rouge/data" -c 95 -r 1000 -n 2 -m -a -l 100 -x /usr/local/share/mead/rouge/kraken/auto_temp.xml > '.$evalResultPath);
+    echo shell_exec('cd /usr/local/share/mead/rouge/playground && perl /usr/local/share/mead/rouge/ROUGE-1.5.5.pl -e "/usr/local/share/mead/rouge/data" -c 95 -r 1000 -n 2 -a -x -2 4 -U /usr/local/share/mead/rouge/playground/auto_temp.xml > '.$evalResultPath);
+
+    $result = file_get_contents($evalResultPath);
+
+    $score = array($docName);
+    foreach (explode("\n", $result) as $i => $line) {
+        $point = stripos($line, ':');
+
+        if ($point !== false && in_array($i, array(3,7,15))) {
+            $score[] = number_format((double) substr($line, $point+2, 7), 4);
+        }
+    }
+    $scores[] = implode(' & ', $score) . ' \\\\';
 }
+
+natsort($scores);
+echo implode(PHP_EOL, $scores);
 
 function rrmdir($dir) { 
     if (is_dir($dir)) { 
@@ -100,4 +151,4 @@ function rrmdir($dir) {
       reset($objects); 
       rmdir($dir); 
     } 
-} 
+}
